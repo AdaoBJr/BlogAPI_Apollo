794df2dcd3fbe8d5bff628d407193c98e3f9c62c
AdaoBJr <adaobjr@gmail.com>
2021-12-10 09:15:07 -0400

    feat: :sparkles: added mongoose

--- src/startServer.js
+++ src/startServer.js
@@ -1,7 +1,16 @@
 import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
 import 'dotenv/config';
 
 export default function startServer({ typeDefs, resolvers }) {
+  const DB_URL = process.env.DB_URL;
+  const DB_NAME = process.env.DB_NAME;
+
+  mongoose.connect(`${DB_URL}/${DB_NAME}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
   const app = new ApolloServer({ typeDefs, resolvers });
 
   const PORT = process.env.PORT || 3000;
