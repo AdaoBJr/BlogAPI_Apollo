3054e1ceae514bed95cd7f132a18e31fbcdf8bc4
AdaoBJr <adaobjr@gmail.com>
2021-12-11 08:48:52 -0400

    feat: :art: comment code yet not used

--- src/graphql/modules/categories/resolvers.js
+++ src/graphql/modules/categories/resolvers.js
@@ -1,17 +1,17 @@
 import Category from '../../../models/category';
 
 export default {
-  Category: {
-    name: async (category) => await Category.findById(category.id),
-  },
+  // Category: {
+  //   name: async (category) => await Category.findById(category.id),
+  // },
   Query: {
     categories: async () => await Category.find(),
     category: async (_, { id }) => await Category.findById(id),
   },
   Mutation: {
-    createCategory: async (_, { data }) => await Category.create(data),
-    updateCategory: async (_, { id, data }) =>
-      await Category.findOneAndUpdate(id, data, { new: true }),
+    createCategory: async (_, { name }) => await Category.create({ name }),
+    updateCategory: async (_, { id, name }) =>
+      await Category.findOneAndUpdate(id, name, { new: true }),
     deleteCategory: async (_, { id }) => !!(await Category.findOneAndDelete(id)),
   },
 };
